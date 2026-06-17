59de4ea00bf5ee6d30498e9d160519fe487a82ea
eg-ossas <alo.isla@duocuc.cl>
2026-06-17 15:41:12 -0400

    creacion de opcion 1 y diccionario

--- prueba1.2.py
+++ prueba1.2.py
@@ -24,13 +24,32 @@ def solicitar_opciones():
     return opcion
 
 def opcion_1(datos_mascotas):
-    nombre_masc = input("Ingrese el nombre de la mascota: ")
-    if " " not in nombre_masc:
-        especie = input("Ingrese la especie de su mascota: (Perro, Gato, Ave)\n").lower()
-        if especie in ("perro", "gato", "ave"):
+    mascota = {}
+    mascota["nombre_masc"] = input("Ingrese el nombre de la mascota: ")
+    if " " not in mascota["nombre_masc"]:
+        mascota["especie"] = input("Ingrese la especie de su mascota: (Perro, Gato, Ave)\n").lower()
+        if mascota["especie"] in ("perro", "gato", "ave"):
             while True:
                 try:
-                    edad
+                    mascota["edad"] = int(input("Ingrese la edad de su mascota: "))
+                    if mascota["edad"] <= 0:
+                        print("Su mascota debe tener al menos 1 año de edad")
+                    else:
+                        break
+                except ValueError:
+                    print("Debe introducir una edad valida")
+    datos_mascotas.append(mascota)
+    return datos_mascotas
+
+def opcion_2(datos_mascotas, mascotas["nombre_masc"]):
+    for i in datos_mascotas():
+
+
+def opcion_3():
+
+def opcion_4():
+
+def opcion_5():
 
 #Codigo Principal
 #Declarar lista mascotas
@@ -41,3 +60,4 @@ op = 0
 while op != 6:
     mostrar_menu()
     op = solicitar_opciones()
+    lista = opcion_1(datos_mascotas)
